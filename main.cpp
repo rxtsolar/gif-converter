91c663858966af753fd46676de5abafe9b96c580
Guangyu Sun <guangyu.sun@oracle.com>
2014-04-01 21:07:51 -0700

    added simple color map to make it work
    Signed-off-by: Guangyu Sun <guangyu.sun@oracle.com>

--- main.cpp
+++ main.cpp
@@ -5,6 +5,26 @@
 using namespace std;
 using namespace cv;
 
+static GifColorType EGAPalette[] =      /* Default color map is EGA palette. */
+{
+    {   0,   0,   0 },   /* 0. Black */
+    {   0,   0, 170 },   /* 1. Blue */
+    {   0, 170,   0 },   /* 2. Green */
+    {   0, 170, 170 },   /* 3. Cyan */
+    { 170,   0,   0 },   /* 4. Red */
+    { 170,   0, 170 },   /* 5. Magenta */
+    { 170, 170,   0 },   /* 6. Brown */
+    { 170, 170, 170 },   /* 7. LightGray */
+    {  85,  85,  85 },   /* 8. DarkGray */
+    {  85,  85, 255 },   /* 9. LightBlue */
+    {  85, 255,  85 },   /* 10. LightGreen */
+    {  85, 255, 255 },   /* 11. LightCyan */
+    { 255,  85,  85 },   /* 12. LightRed */
+    { 255,  85, 255 },   /* 13. LightMagenta */
+    { 255, 255,  85 },   /* 14. Yellow */
+    { 255, 255, 255 },   /* 15. White */
+};
+
 class GifConverter {
 public:
 	GifConverter(const char* fileName)
@@ -22,6 +42,7 @@ public:
 	{
         int ret;
 		GifFileType* gif = EGifOpenFileName(fileName, false, 0);
+        ColorMapObject* colorMap = GifMakeMapObject(16, EGAPalette);
         if (!gif) {
             cerr << "EGifOpenFileName failed: " << GifErrorString(gif->Error) << endl;
             exit(1);
@@ -29,7 +50,7 @@ public:
 		GifPixelType* data;
 
 		EGifSetGifVersion(gif, true);
-		ret = EGifPutScreenDesc(gif, image.cols, image.rows, 0, 0, 0);
+		ret = EGifPutScreenDesc(gif, image.cols, image.rows, 0, 0, colorMap);
         if (ret == GIF_ERROR) {
             cerr << "EGifPutScreenDesc failed: " << GifErrorString(gif->Error) << endl;
             exit(1);
@@ -56,7 +77,7 @@ private:
 int main(int argc, char* argv[])
 {
     GifConverter gc("aaa_700.rnd.png");
-    //gc.showImage();
+    gc.showImage();
     gc.writeGif("output.gif");
 	return 0;
 }
